d96733822a0410963d46bc3096e53e4053090066
hhhhnFe <rlaguscjf0902@gmail.com>
2023-07-02 13:38:51 +0900

    PMF/CPFM complete

--- PMF/config.py
+++ PMF/config.py
@@ -9,16 +9,16 @@ def config_parser():
     parser.add_argument('--datadir', type=str, help='input data directory')
 
     parser.add_argument('--split', type=str, help='split type')
-    parser.add_argument('--batch_sizee', type=int, default=256, help='batch size')
-    parser.add_argument('--shuffle', type=bool, default=True, help='whether to shuffle batch')
+    parser.add_argument('--batch_size', type=int, default=256, help='batch size')
+    parser.add_argument('--shuffle', action='store_true', help='whether to shuffle batch')
     parser.add_argument('--N_epoch', type=int, default=100, help='number of epochs')
 
     parser.add_argument('--D', type=int, default=30, help='split dimension')
     parser.add_argument('--N', type=int, help='number of users')
     parser.add_argument('--M', type=int, help='number of movies')
 
-    parser.add_argument('--constrained', type=bool, default=False, help='constrained or not')
-    parser.add_argument('--adaptive', type=bool, default=False, help='adaptive or not')
+    parser.add_argument('--constrained', action='store_true', help='constrained or not')
+    parser.add_argument('--adaptive', action='store_true', help='adaptive or not')
     parser.add_argument('--lu', type=float, default=0.01, help='lambda_u')
     parser.add_argument('--lv', type=float, default=0.1, help='lambda_v')
 
--- PMF/train.py
+++ PMF/train.py
@@ -1,37 +1,64 @@
 import sys,os
 import torch
-from torch.utils.data import Dataloader
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm, trange
 
 import ml_data
-import config
 
-from .ml_data import movie_lens
+from ml_data import movie_lens
+from config import config_parser
+
+ratings_loss = lambda x, y: .5 * torch.sum((x - y)**2)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def train():
     parser = config_parser()
     args = parser.parse_args()  
+    writer = SummaryWriter(args.basedir)
 
     dataset = movie_lens(args.datadir, args.split)
-    dataloader = Dataloader(dataset=dataset, batch_size=args.batch_size,
+    dataloader = DataLoader(dataset=dataset, batch_size=args.batch_size,
                             shuffle=args.shuffle, drop_last=False)
     
+    V = torch.rand(size=(args.D, args.M), device=device, requires_grad=True)
+    Y = torch.rand(size=(args.D, args.N), device=device, requires_grad=True)
     if args.constrained == False:
-        U = torch.rand(size=(args.D, args.N), device=device, requires_grad=True)
-        V = torch.rand(size=(args.D, args.M), device=device, requires_grad=True)
-        
-        for i in trange(args.N_epoch):
-            for batch in tqdm(dataloader):
-                users = batch['user']
-                movies = batch['movie']
-                ratings = batch['rating']
+        W = torch.zeros_like(V, device=device)
     else:
-        Y = torch.rand(size=(args.D, args.N), device=device, requires_grad=True)
         W = torch.rand(size=(args.D, args.M), device=device, requires_grad=True)
-        V = torch.rand(size=(args.D, args.M), device=device, requires_grad=True)
+    
+    optimizer = torch.optim.Adam([V, Y, W], lr=1e-3)
+
+    for i in trange(args.N_epoch):
+        for batch in dataloader:
+            optimizer.zero_grad() 
+
+            users = batch['user'].to(device)
+            movies = batch['movie'].to(device)
+            ratings = batch['rating'].to(device)
+
+            U = Y[:, users] + W[:, movies]
+            U = torch.transpose(U, 0, 1) # U : (batch_size) * D
+            V_ = V[:, movies]            # V_ : D * (batch_size)
+            
+            UTV = torch.diagonal(torch.mm(U, V_)).to(device)
+            loss = ratings_loss(UTV, ratings) + args.lu * torch.norm(U, p='fro', dim=1).sum() \
+                                             + args.lv * torch.norm(V_, p='fro', dim=0).sum()
+
+            loss.backward()
+            optimizer.step()
+        
+        RMSE = ratings_loss(UTV, ratings).sqrt()
+        writer.add_scalar("RMSE/train", RMSE, i)
+        if i%5 == 0:
+            tqdm.write(f"[TRAIN] Iter: {i} RMSE: {RMSE}")
+    
+    writer.flush()
+    writer.close()
 
 if __name__ == '__main__':
-    torch.set_default_tensor_type('torch.cuda.FloatTensor')
+    #torch.set_default_device(device)
     train()
+    
