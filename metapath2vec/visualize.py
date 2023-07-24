30c1eaddd12324140a7e594ec1a31b373e0d1ee4
Hyunchul Kim <rlaguscjf0902@gmail.com>
2023-07-24 11:24:17 +0900

    added visualizer

---
+++ metapath2vec/visualize.py
@@ -0,0 +1,40 @@
+import sys,os
+
+from config import config_parser
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch.utils.tensorboard import SummaryWriter
+from torch_geometric.datasets import AMiner
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+@torch.no_grad()
+def test(args):
+    path = os.path.join(args.basedir, args.expname, args.testname)
+    model = torch.load(path)
+    
+
+    if args.dataset == 'AMiner':
+        dataset = AMiner(root=args.datadir)
+    else:
+        raise NotImplementedError
+
+    test_embeddings = model['embedding'][dataset[0]['author']['y_index']]
+    test_labels = dataset[0]['author']['y']
+
+    writer = SummaryWriter(path)
+    writer.add_embedding(
+        test_embeddings,
+        test_labels
+    )
+    
+    writer.close()
+
+if __name__ == '__main__':
+    parser = config_parser()
+    args = parser.parse_args()
+
+    test(args)
